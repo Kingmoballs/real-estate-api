618e62d9b8867f335781a2be5816b255445dff6f
Mobolaji Samuel <141836780+Kingmoballs@users.noreply.github.com>
2025-12-15 15:55:45 +0100

    Update propertyValidator.js

--- validators/propertyValidator.js
+++ validators/propertyValidator.js
@@ -24,12 +24,20 @@ const updatePropertySchema = Joi.object({
     title: Joi.string().min(3).trim().max(200).required(),
     description: Joi.string().min(10).required(),
     location:Joi.string().required(),
-    price: Joi.number().positive().required(),
+    price: Joi.when("propertyType", {
+        is: "sale",
+        then: Joi.number().positive().required(),
+        otherwise: Joi.forbidden(),
+    }),
     propertyType: Joi.string().valid("sale", "serviced").required(),
     bedrooms: Joi.number().integer().min(0),
     bathrooms: Joi.number().integer().min(0),
     images: Joi.array().items(Joi.string().uri()).min(1).required(),
-    dailyRate: Joi.number().positive().required(),
+    dailyRate: Joi.when("propertyType", {
+        is: "serviced",
+        then: Joi.number().positive().required(),
+        otherwise: Joi.forbidden(),
+    })
 
 })
 
