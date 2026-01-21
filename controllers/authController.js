36b8f4317c529590d608e19abf383565ebacbfd2
Mobolaji Samuel <141836780+Kingmoballs@users.noreply.github.com>
2026-01-22 00:59:24 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,6 @@
-const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-
 const authService = require("../services/authService")
 
-const generateToken = (userId)  => {
-    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" })
-};
 
 // @route   POST /api/auth/register
 exports.register = async (req, res, next) => {
